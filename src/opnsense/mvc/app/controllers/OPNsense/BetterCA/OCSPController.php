34b336d49d99ced3d4f0a2a5d1762bad10986938
Måns Abrahamsson <mans.abrahamsson@gmail.com>
2025-03-18 10:41:45 +0100

    Added OCSP settings page (without any options)

---
+++ src/opnsense/mvc/app/controllers/OPNsense/BetterCA/OCSPController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OPNsense\BetterCA;
+
+/**
+ * Class IndexController
+ * @package OPNsense\BetterCA
+ */
+class OCSPController extends \OPNsense\Base\IndexController
+{
+    public function indexAction()
+    {
+        // pick the template to serve to our users.
+        $this->view->pick('OPNsense/BetterCA/ocsp');
+        
+        // $this->view->generalForm = $this->getForm("general");
+    }
+}
